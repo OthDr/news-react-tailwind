52a207920833f4416e6815048e54995d1c51c426
unknown <otmax.vft@gmail.com>
2022-08-12 23:55:58 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App flex items-center bg-zinc-100 p-4 m-12 shadow-2xl shadow-amber-200  rounded-lg hover:scale-105 duration-200'>
+      <h1 className="mx-auto text-4xl text-amber-400 font-bold border-4 border-amber-400 rounded-xl py-2 px-4">Doth News</h1>
+    </div>
+  );
+}
+
+export default App;
